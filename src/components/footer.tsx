412acd54978044b2e2dd132de1554fd8517ed821
Gabriell Bastos <gabriellbastos0825@gmail.com>
2025-08-04 14:51:27 +0000

    errei o nome, o veradeiro é LOGO HORIZONTAL.svg

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-primary text-primary-foreground py-4">
       <div className="container mx-auto px-4 md:px-6 flex flex-col items-center justify-center gap-4">
-        <Image src="/LOGO%20RETANGULAR.svg" alt="CP Marketing Logo" width={120} height={40} data-ai-hint="logo" />
+        <Image src="/LOGO%20HORIZONTAL.svg" alt="CP Marketing Logo" width={120} height={40} data-ai-hint="logo" />
         <p className="text-sm text-center">
           Criado por <Link href="#" className="underline">CP Marketing</Link>
         </p>
