d470386507d373e09403fde1c3ed9427ad687b84
Gabriell Bastos <gabriellbastos0825@gmail.com>
2025-08-06 16:50:50 +0000

    adicione esse código de monitoramento no site todo:
    <script type="text/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { Montserrat } from 'next/font/google';
+import Script from 'next/script';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -23,6 +24,11 @@ export default function RootLayout({
       <body className={`${montserrat.variable} font-body antialiased`}>
         {children}
         <Toaster />
+        <Script
+          type="text/javascript"
+          src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/4b29f569-0782-4731-9daa-cd08177a5ae8-loader.js"
+          async
+        />
       </body>
     </html>
   );
