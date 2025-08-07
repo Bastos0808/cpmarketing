64728a52faac36e9bc09005250f2f245f153523a
Gabriell Bastos <gabriellbastos0825@gmail.com>
2025-08-07 17:29:57 +0000

    ajuste esse espaço que esta sobreando no topo da página home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import ModernNav from "./modern-nav"
 
 export default function Header() {
   return (
-    <header className="w-full z-50">
+    <header className="absolute top-0 left-0 w-full z-50">
       <div className="container mx-auto px-4 md:px-6 flex h-28 items-center justify-between">
           <Link href="/" className="flex items-center gap-2">
             <Image src="/LOGO%20REDONDA.svg" alt="CP Marketing Logo" width={60} height={60} data-ai-hint="logo" />
