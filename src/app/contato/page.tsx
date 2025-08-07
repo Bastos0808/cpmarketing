cb5bd6002faf632bc0156e687ea519dbc348905d
Gabriell Bastos <gabriellbastos0825@gmail.com>
2025-08-07 17:35:02 +0000

    tem um espaço grande entre o cabeçalho e o texto h2 (headline), quero qu

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -6,7 +6,7 @@ export default function Contato() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
-      <main className="flex-grow pt-28">
+      <main className="flex-grow pt-20">
         <ContactPage />
       </main>
       <Footer />
